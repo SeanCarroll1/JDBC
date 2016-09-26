65adfb05612dd5ebffcd2449f3b1caab15640a6e
SeanCarroll1 <carroll.seann@gmail.com>
2016-09-26 17:25:47 +0100

    Update

--- src/Database/DropDatabase.java
+++ src/Database/DropDatabase.java
@@ -1,56 +1,22 @@
-package Database;
+public class Solution {
+	public int removeDuplicates(ArrayList<Integer> a) 
+	{
+	  
+	  int index = 1;
 
-import java.sql.*;
+		if (a == null || a.size() == 0)
+			return 0;
 
+		for (int i = 1; i < a.size(); i++) {
 
-	public class DropDatabase {
-	   // JDBC driver name and database URL
-	   static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
-	   static final String DB_URL = "jdbc:mysql://localhost/";
+			if (!a.get(i).equals(a.get(i - 1)))
+			{
 
-	   //  Database credentials
-	   static final String USER = "root";
-	   static final String PASS = "";
-	   
-	   public static void main(String[] args) {
-	   Connection conn = null;
-	   Statement stmt = null;
-	   try{
-	      //STEP 2: Register JDBC driver
-	      Class.forName("com.mysql.jdbc.Driver");
+				a.set(index, a.get(i));
+				index++;
+			}
+		}
 
-	      //STEP 3: Open a connection
-	      System.out.println("Connecting to a selected database...");
-	      conn = DriverManager.getConnection(DB_URL, USER, PASS);
-	      System.out.println("Connected database successfully...");
-	      
-	      //STEP 4: Execute a query
-	      System.out.println("Deleting database...");
-	      stmt = conn.createStatement();
-	      
-	      String sql = "DROP DATABASE STUDENTS";
-	      stmt.executeUpdate(sql);
-	      System.out.println("Database deleted successfully...");
-	   }catch(SQLException se){
-	      //Handle errors for JDBC
-	      se.printStackTrace();
-	   }catch(Exception e){
-	      //Handle errors for Class.forName
-	      e.printStackTrace();
-	   }finally{
-	      //finally block used to close resources
-	      try{
-	         if(stmt!=null)
-	            conn.close();
-	      }catch(SQLException se){
-	      }// do nothing
-	      try{
-	         if(conn!=null)
-	            conn.close();
-	      }catch(SQLException se){
-	         se.printStackTrace();
-	      }//end finally try
-	   }//end try
-	   System.out.println("Goodbye!");
+		return index;
 	}
-	}
+}
